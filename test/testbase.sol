0d2c2c3b199bdd3aa2620d3c0051a87252138b1e
Park Jeong Won <parkjeongwon@MacBook-Pro-374.local>
2022-12-14 20:56:45 +0900

    latest

---
+++ test/testbase.sol
@@ -0,0 +1,245 @@
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "src/protocol/controller.sol";
+import {MarketManager} from "src/protocol/marketmanager.sol";
+import {ReputationNFT} from "src/protocol/reputationtoken.sol";
+import {Cash} from "src/utils/Cash.sol";
+import {CreditLine, MockBorrowerContract} from "src/vaults/instrument.sol";
+import {SyntheticZCBPoolFactory} from "src/bonds/synthetic.sol"; 
+import {LinearCurve} from "src/bonds/GBC.sol"; 
+import {FixedPointMath} from "src/bonds/libraries.sol"; 
+import {CoveredCallOTC} from "src/vaults/dov.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {SimpleNFTPool} from "src/vaults/nftLending.sol"; 
+
+contract TestBase is Test {
+    using FixedPointMath for uint256; 
+    using stdStorage for StdStorage; 
+
+    Controller controller;
+    MarketManager marketmanager;
+    Cash collateral;
+    VaultFactory vaultFactory;
+    SyntheticZCBPoolFactory poolFactory; 
+    Cash collateral2; 
+    CoveredCallOTC otc; 
+    MockBorrowerContract borrowerContract = new MockBorrowerContract();
+    CreditLine instrument;
+    SimpleNFTPool nftPool; 
+
+    address deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+    uint256 unit = 10**18; 
+    uint256 constant precision = 1e18;
+    address vault_ad; 
+
+    // Participants 
+    address jonna;
+    address jott; 
+    address gatdang;
+    address sybal; 
+    address chris; 
+    address miku;
+    address tyson; 
+    address yoku;
+    address toku; 
+    address goku; 
+
+    // Varaibles that sould be tinkered
+    uint256 principal = 1000 * precision;
+    uint256 interest = 100*precision; 
+    uint256 duration = 1*precision; 
+    uint256 faceValue = 1100*precision; 
+    uint256 N = 1;
+    uint256 sigma = precision/20; //5%
+    uint256 alpha = precision*4/10; 
+    uint256 omega = precision*2/10;
+    uint256 delta = precision*2/10; 
+    uint256 r = 0;
+    uint256 s = precision*2;
+    uint256 steak = precision;
+    uint256 amount1; 
+    uint256 amount2; 
+    uint256 amount3; 
+    uint256 amount4; 
+
+    uint256 strikeprice = precision; 
+    uint256 pricePerContract = precision/10; //pricepercontract * 
+    uint256 shortCollateral = principal; 
+    uint256 longCollateral = shortCollateral.mulWadDown(pricePerContract); 
+
+    function setUsers() public {
+        jonna = address(0xbabe);
+        vm.label(jonna, "jonna"); // manager1
+        jott = address(0xbabe2); 
+        vm.label(jott, "jott"); // utilizer 
+        gatdang = address(0xbabe3); 
+        vm.label(gatdang, "gatdang"); // validator
+        sybal = address(0xbabe4);
+        vm.label(sybal, "sybal");//manager2
+        chris=address(0xbabe5);
+        vm.label(chris, "chris");//manager3
+        miku = address(0xbabe6);
+        vm.label(miku, "miku"); //manager4
+        goku = address(0xbabe7); 
+        vm.label(goku, "goku"); //LP1 
+        toku = address(0xbabe8);
+        vm.label(toku, "toku"); 
+
+        controller._incrementScore(jonna, precision);
+        controller._incrementScore(jott, precision);
+        controller._incrementScore(gatdang, precision);
+        controller._incrementScore(sybal, precision);
+        controller._incrementScore(chris, precision);
+        controller._incrementScore(miku, precision);
+        controller._incrementScore(goku, precision);
+        controller._incrementScore(toku, precision);
+
+        vm.prank(jonna); 
+        collateral.faucet(10000000*precision);
+        vm.prank(jott); 
+        collateral.faucet(10000000*precision);
+        vm.prank(gatdang); 
+        collateral.faucet(10000000*precision); 
+        vm.prank(sybal); 
+        collateral.faucet(10000000*precision); 
+        vm.prank(chris); 
+        collateral.faucet(10000000*precision); 
+        vm.prank(miku); 
+        collateral.faucet(10000000*precision); 
+        vm.prank(goku);
+        collateral.faucet(10000000*precision); 
+        vm.prank(toku);
+        collateral.faucet(10000000*precision);
+
+        vm.prank(toku); 
+        controller.testVerifyAddress(); 
+        vm.prank(jott);
+        controller.testVerifyAddress();
+        vm.prank(jonna);
+        controller.testVerifyAddress();
+        vm.prank(gatdang); 
+        controller.testVerifyAddress(); 
+        vm.prank(chris); 
+        controller.testVerifyAddress(); 
+        vm.prank(miku); 
+        controller.testVerifyAddress(); 
+        vm.prank(sybal); 
+        controller.testVerifyAddress();
+    }
+
+   
+
+    function initiateCreditMarket() public {
+        Vault.InstrumentData memory data;
+
+        data.trusted = false; 
+        data.balance = 0;
+        data.faceValue = faceValue;
+        data.marketId = 0; 
+        data.principal = principal;
+        data.expectedYield = interest;
+        data.duration = duration;
+        data.description = "test";
+        data.Instrument_address = address(instrument);
+        data.instrument_type = Vault.InstrumentType.CreditLine;
+        data.maturityDate = 10; 
+
+        controller.initiateMarket(jott, data, 1);
+        uint256[] memory words = new uint256[](N);
+        for (uint256 i=0; i< words.length; i++) {
+            words[i] = uint256(keccak256(abi.encodePacked(i)));
+        }
+        marketmanager.fulfillRandomWords(1, words);
+    }
+
+    function initiateOptionsOTCMarket() public{
+        Vault.InstrumentData memory data;
+        data.trusted = false; 
+        data.balance = 0;
+        data.faceValue = faceValue;
+        data.marketId = 0; 
+        data.principal = principal;
+        data.expectedYield = interest;
+        data.duration = duration;
+        data.description = "test";
+        data.Instrument_address = address(otc);
+        data.instrument_type = Vault.InstrumentType.CoveredCall;
+        data.maturityDate = 10; 
+        controller.initiateMarket(toku, data, 1);
+        uint256[] memory words = new uint256[](N);
+        for (uint256 i=0; i< words.length; i++) {
+            words[i] = uint256(keccak256(abi.encodePacked(i)));
+        }
+        marketmanager.fulfillRandomWords(1, words);
+    }
+
+    function initiateSimpleNFTLendingPool() public {
+        Vault.InstrumentData memory data; 
+        Vault.PoolData memory poolData; 
+
+        poolData.saleAmount = principal; 
+        poolData.initPrice = 7e17; 
+        poolData.promisedReturn = 3000000000; 
+        poolData.inceptionTime = block.timestamp; 
+        poolData.inceptionPrice = 8e17; 
+        poolData.leverageFactor = 3; 
+
+        data.isPool = true; 
+        data.trusted = false; 
+        data.balance = 0;
+        data.faceValue = 0;
+        data.marketId = 0; 
+        data.principal = 0;
+        data.expectedYield = 0;
+        data.duration = 0;
+        data.description = "test";
+        data.Instrument_address = address(nftPool);
+        data.instrument_type = Vault.InstrumentType.Pool;
+        data.maturityDate = 0; 
+        data.poolData = poolData; 
+
+        controller.initiateMarket(toku, data, 1); 
+
+        uint256[] memory words = new uint256[](N);
+        for (uint256 i=0; i< words.length; i++) {
+            words[i] = uint256(keccak256(abi.encodePacked(i)));
+        }
+        marketmanager.fulfillRandomWords(1, words);
+
+    }
+
+    function doApproveCol(address _who, address _by) public{
+        vm.prank(_by); 
+        collateral.approve(_who, type(uint256).max); 
+    }
+    function doInvest(address vault, address _by, uint256 amount) public{
+        vm.prank(_by); 
+        Vault(vault).deposit(amount, _by); 
+    }
+    function doApproveVault(address vault, address _from, address _to) public {
+        vm.prank(_from);
+        ERC20(vault).approve(_to, type(uint256).max);
+    }
+
+    function doMint(address vault, address _by, uint256 shares) public {
+        vm.prank(_by);
+        Vault(vault).mint(shares, _by);
+    }
+    function cBal(address _who) public returns(uint256) {
+        return collateral.balanceOf(_who); 
+    }
+    function doApprove(uint256 marketId, address vault) public{
+        // validators invest and approve 
+        address[] memory vals = marketmanager.viewValidators(marketId);
+        uint256 initialStake = marketmanager.getInitialStake(marketId);
+        for (uint i=0; i < vals.length; i++) {
+            doApproveCol(vault, vals[i]);
+            doApproveVault(vault, vals[i], address(marketmanager));
+            doApproveCol(address(marketmanager), vals[i]);
+            doMint(vault, vals[i], initialStake);
+            vm.prank(vals[i]);
+            marketmanager.validatorApprove(marketId);
+        }
+    }
+   
+}
